fae88993db8fa6369068d56b0ea1b8bccb4282a1
Igor-from-Kyiv <igorkramarenko86@gmail.com>
2025-05-19 21:23:12 +0000

    files update

--- fastApi_backend/main.py
+++ fastApi_backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 import models, db_config
 from db_config import Base, engine, Session
@@ -20,12 +20,15 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
+
+
+
 @app.get("/")
 async def root():
     return {"message": "test..."}
 
 @app.get("/items/")
-def read_items(db: Session = Depends(db_config.get_db)):
+async def read_items(db: Session = Depends(db_config.get_db)):
     items = db.query(models.Item).all()
     print("items: {items}")
     return items
@@ -33,3 +36,13 @@ def read_items(db: Session = Depends(db_config.get_db)):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return({"item_id": item_id, "message": "respose from backend"})
+
+@app.post("/new_item/")
+async def write_item(request: Request):
+    print(f"data: {request}")
+    print(f"data: {request.__dict__}")
+    print(f"body: {request.body()}")
+    print(f"json: {request.json()}")
+    print(f"url: {request.url}")
+    #print("data: {data}")
+    return
